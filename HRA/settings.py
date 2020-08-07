899dd1c7a49b51a520a196b8df2da2de0967bc52
Satish Pokala <satishpokala124@gmail.com>
2020-08-07 19:37:43 +0530

    Hiding the SECRET_KEY and setting Connecting to Postgres Database of Heroku

--- HRA/settings.py
+++ HRA/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't!dxvngv4hht0a_w7ysn((nb7w0x)#352e@%it)9u4%h($1gs+'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -82,6 +82,10 @@ DATABASES = {
     }
 }
 
+# For connecting with Heroku Postgres Database
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
